de7beaeb7ddfbce1e959d80b0af965288d6aef39
og1474 <o.g.r.m.g.m@gmail.com>
2025-06-09 21:24:39 +0900

    データの削除機能を追加

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask,render_template,request,redirect
 import sqlite3
 from datetime import datetime
 
+
 app = Flask(__name__)
 HOURLY_WAGE = 1100 ##時給（1100円で固定）
+FARE = 200 ##交通費（とりあえず固定）
 
 def init_db():
-    conn = sqlite3.connect("shift.db")
+    conn = sqlite3.connect('shift.db')
     c = conn.cursor() ##すでにある場合は上書きしない
     c.execute('''
               CREATE TABLE IF NOT EXISTS shifts ( 
@@ -24,20 +26,27 @@ def index():
     c = conn.cursor()
 
     if request.method == 'POST': ##ユーザーがフォームを送信=POSTした時に実行
-        date = request.form['date'] ##dateで入力された日付を取得
-        hours = float(request.form['hours']) ##hoursで入植された数字を取得して小数に変換
-        c.execute('INSERT INTO shifts (date,hours) VALUES(?,?)',(date,hours)) ##テーブルに保存
-        conn.commit()
+        action = request.form.get('action') ##valueの値を取得
+        if action == 'add':
+            date = request.form['date'] ##dateで入力された日付を取得
+            hours = float(request.form['hours']) ##hoursで入植された数字を取得して小数に変換
+            c.execute('INSERT INTO shifts (date,hours) VALUES(?,?)',(date,hours)) ##テーブルに保存
+            conn.commit()
+        elif action == 'delete':
+            delete_id = request.form['id'] ##削除するidを取得
+            c.execute('DELETE FROM shifts WHERE id = ?',(delete_id,)) ##指定されたidのデータを削除
+            conn.commit()
     
-    c.execute('SELECT date,hours FROM shifts') ##データベースから記録を取得
+    c.execute('SELECT date,hours,id FROM shifts') ##データベースから記録を取得
     data = c.fetchall()
 
     #合計給与を計算
     total_hours = sum(row[1] for row in data) ##1行ごとに2つめの値を足す
-    total_salary = total_hours * HOURLY_WAGE
+    total_days = sum(1 for row in data)
+    total_salary = (total_hours * HOURLY_WAGE)+(total_days * FARE)
 
     conn.close()        
-    return render_template("index.html",data=data,total=total_salary) ##salaryにsalaryを代入しindex.htmlを返す
+    return render_template('index.html',data=data,total=total_salary,HOURLY_WAGE=HOURLY_WAGE,FARE=FARE) ##salaryにsalaryを代入しindex.htmlを返す
 
 if __name__ == '__main__': ##このファイルを直接実行したときだけFlaskを起動
     init_db()
